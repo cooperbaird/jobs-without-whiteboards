1d053913fae5f512c09a410af34a7350a233e132
Cooper Baird <cooperbaird@gmail.com>
2021-06-06 14:11:56 -0400

    No partial word matches

--- list_companies.py
+++ list_companies.py
@@ -1,8 +1,8 @@
-import requests, sys
+import re, requests, sys
 
 companies = requests.get('https://raw.githubusercontent.com/poteto/hiring-without-whiteboards/master/README.md').text.split('\n')
 del companies[:companies.index('## A - C')]
 del companies[companies.index('## Also see:'):]
-companies = list(filter(lambda item: any(keyword.lower() in item.lower() for keyword in sys.argv[1:]), companies))
-companies = list(map(lambda item: item[item.index('[')+1:item.index(']')], companies))
+companies = list(filter(lambda item: any(re.search(r'\b' + keyword + r'\b', item, flags=re.IGNORECASE) for keyword in sys.argv[1:]), companies))
+companies = list(map(lambda item: item[item.index('[') + 1:item.index(']')], companies))
 print(*companies, sep='\n')
